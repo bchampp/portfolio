80c44c02948ff85dd921929b848a24b6d3d485ba
Brent Champion <brent.champion@queensu.ca>
2020-06-03 00:13:34 -0400

    playing around with nextjs

---
+++ next-version/pages/Components/Projects.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+
+    if(this.props.data){
+      var projects = this.props.data.projects.map(function(projects){
+        var projectImage = 'images/portfolio/'+projects.image;
+        return <div key={projects.title} className="columns projects-item">
+           <div className="item-wrap">
+            <a href={projects.url} title={projects.title}>
+               <img alt={projects.title} src={projectImage} />
+               <div className="overlay">
+                  <div className="projects-item-meta">
+                 <h5>{projects.title}</h5>
+                     <p>{projects.category}</p>
+                  </div>
+                </div>
+              <div className="link-icon"><i className="fa fa-link"></i></div>
+            </a>
+          </div>
+        </div>
+      })
+    }
+
+    return (
+      <section id="projects">
+      <div className="row">
+         <div className="twelve columns collapsed">
+            <h1>Here are some projects I've worked on recently. </h1>
+            <div id="projects-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                {projects}
+            </div>
+            <h1>To see all my projects, check out my <a href='https://github.com/bchampp'>GitHub</a></h1>
+          </div>
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Projects;
---
+++ next-version/pages/index.js
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+import { Component } from 'react';
+import $ from 'jquery';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Projects from './Components/Projects';
+
+export default class extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      resumeData: {}
+    };
+  }
+
+  getResumeData(){
+    $.ajax({
+      url:'/resumeData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.getResumeData();
+  }
+
+  render() {
+    return (
+      <Head>
+      <div className="App">
+          <Header data={this.state.resumeData.main}/>
+          <About data={this.state.resumeData.main}/>
+          <Projects data={this.state.resumeData.portfolio}/>
+          <Resume data={this.state.resumeData.resume}/>
+          <Footer data={this.state.resumeData.main}/>
+      </div>
+      </Head>
+    );
+  }
+}
