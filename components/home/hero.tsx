9814cd94d625409c4a9faaaef085bbd049233417
Brent Champion <brent.champion@queensu.ca>
2020-10-27 19:53:39 -0400

    updates typing

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -6,19 +6,19 @@ export default function Hero() {
 		<div className="rounded-lg shadow-lg relative">
 			<img className="w-full object-cover" src="/images/background.jpg" alt="Mountain Image" />
 			<div className="px-6 py-4 m-auto md:flex items-center absolute top-0 left-0 right-0 bottom-0">
-				<h2 className="font-semibold text-white tracking-tight text-5xl w-full mb-3 items-center text-center align-middle">
+				<h3 className="font-semibold text-white tracking-tight text-6xl w-full mb-3 items-center text-center align-middle">
 					<Typewriter options={{loop: true}}
 						onInit={(typewriter) => {
 							typewriter
-								.typeString("Hi! I'm Brent.\n")
-                .pauseFor(1500)
-                .typeString("I like building things")
-                .pauseFor(3000)
+								.typeString("Hi, I'm Brent.\n")
+                .pauseFor(2000)
+                .typeString("I like to build things.")
+                .pauseFor(2000)
 								.deleteAll()
                 .start();
 						}}
 					/>
-				</h2>
+				</h3>
 			</div>
 		</div>
 	);
