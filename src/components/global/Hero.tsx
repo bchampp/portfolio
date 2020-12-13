1a48787d91c5123ac40ba16931937a76cfc0a72a
Brent Champion <brent.champion@queensu.ca>
2020-12-13 14:11:05 -0500

    implements fix for broken div on landing page

--- src/components/global/Hero.tsx
+++ src/components/global/Hero.tsx
@@ -1,10 +1,21 @@
 /* Hero Component */
 
+import { isWidthDown } from '@material-ui/core';
+import { useEffect } from 'react';
 import styles from '../../styles/hero.module.css';
 
 const Typewriter = require('typewriter-effect');
 
 export default function Hero() {
+	useEffect(() => {
+		// Hide the broken white square that appears beside the coffee widget
+		function hideBrokenCoffeeChild(){
+			let widget = document.getElementById('bmc-wbtn').nextSibling;
+			widget.parentNode.removeChild(widget); 
+		}
+		hideBrokenCoffeeChild();
+	}, []);
+
 	return (
 		<div className={styles.hero}>
 			{/* Buy Me A Coffee!! */}
